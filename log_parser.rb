e891309fad63d39dbecc3cbebfb3c980f8d581d0
Ahsanuzzaman Khan <webdevahsan@gmail.com>
2016-08-23 12:11:19 +0600

    log_parser.rb - remove unusual lines

--- log_parser.rb
+++ log_parser.rb
@@ -1,131 +1,120 @@
-require './math_utils.rb'
+require './math_utils'
 
 class LogfileParser
-    URLREGEX = Regexp.union([
-	/path=\/api\/users\/.*\/count_pending_messages (.*)/, 
-	/path=\/api\/users\/.*\/get_messages (.*)/, 
-	/path=\/api\/users\/.*\/get_friends_progress (.*)/, 
-	/path=\/api\/users\/.*\/get_friends_score (.*)/,
-	/method=POST path=\/api\/users\/\d+ (.*)/,
-	/method=GET path=\/api\/users\/\d+ (.*)/
-	]) 
+    URL_REGEX = Regexp.union([
+                              /path=\/api\/users\/.*\/count_pending_messages (.*)/,
+                              /path=\/api\/users\/.*\/get_messages (.*)/,
+                              /path=\/api\/users\/.*\/get_friends_progress (.*)/,
+                              /path=\/api\/users\/.*\/get_friends_score (.*)/,
+                              /method=POST path=\/api\/users\/\d+ (.*)/,
+                              /method=GET path=\/api\/users\/\d+ (.*)/
+                             ])
     def initialize
         @log = File.open('./sample.log')
         @final_hash = Hash.new(0)
 
         @final_hash['count_pending'] = Hash.new
+        @final_hash['count_pending']['path'] = '/api/users/{user_id}/count_pending_messages'
         @final_hash['count_pending']['dyno'] = Array.new
         @final_hash['count_pending']['response_time'] = Array.new
 
         @final_hash['get_messages'] = Hash.new
+        @final_hash['get_messages']['path'] = '/api/users/{user_id}/get_messages'
         @final_hash['get_messages']['dyno'] = Array.new
         @final_hash['get_messages']['response_time'] = Array.new
 
         @final_hash['friends_progress'] = Hash.new
+        @final_hash['friends_progress']['path'] = '/api/users/{user_id}/get_friends_progress'
         @final_hash['friends_progress']['dyno'] = Array.new
         @final_hash['friends_progress']['response_time'] = Array.new
 
         @final_hash['friends_score'] = Hash.new
+        @final_hash['friends_score']['path'] = '/api/users/{user_id}/get_friends_score'
         @final_hash['friends_score']['dyno'] = Array.new
         @final_hash['friends_score']['response_time'] = Array.new
 
         @final_hash['post_users'] = Hash.new
+        @final_hash['post_users']['path'] = 'POST /api/users/{user_id}'
         @final_hash['post_users']['dyno'] = Array.new
         @final_hash['post_users']['response_time'] = Array.new
 
         @final_hash['get_users'] = Hash.new
+        @final_hash['get_users']['path'] = 'GET /api/users/{user_id}'
         @final_hash['get_users']['dyno'] = Array.new
         @final_hash['get_users']['response_time'] = Array.new
     end
 
     attr_reader :final_hash
 
-    def get_matched_lines &block
-	    # line_num = 0
+    def get_matched_lines
         @log.each_line do |line|
-            if line =~ URLREGEX
-                # print "#{line_num += 1} #{line}"
+            if line =~ URL_REGEX
                 yield line
             end
         end
+        @log.close
     end
 
     def prepare_hash
-       self.get_matched_lines {
-           |l|
-           dyno = c_time = s_time = ''
+       '''
+       Method to prepare our final hash
+       '''
+       self.get_matched_lines { |l|
 
-           dyno = l.match(/dyno=(.*) connect=(.*)ms service=(.*)ms/)[1]
-           c_time = l.match(/dyno=(.*) connect=(.*)ms service=(.*)ms/)[2]
-           s_time = l.match(/dyno=(.*) connect=(.*)ms service=(.*)ms/)[3]
+           matches = l.match(/dyno=(.*) connect=(.*)ms service=(.*)ms/)
+           dyno = matches[1]
+           c_time = matches[2]
+           s_time = matches[3]
 
            if l =~ /path=\/api\/users\/.*\/count_pending_messages (.*)/
 
-               if @final_hash['count_pending']
-                   if @final_hash['count_pending']['dyno']
-                       @final_hash['count_pending']['dyno'].push(dyno.to_s)
-                       @final_hash['count_pending']['response_time'].push(c_time.to_f + s_time.to_f)
-                   end
-               end
+               self.build_hash('count_pending', dyno, (c_time.to_f+s_time.to_f))
            end
 
            if l =~ /path=\/api\/users\/.*\/get_messages (.*)/
 
-               if @final_hash['get_messages']
-                   if @final_hash['get_messages']['dyno']
-                       @final_hash['get_messages']['dyno'].push(dyno)
-                       @final_hash['get_messages']['response_time'].push(c_time.to_f + s_time.to_f)
-                   end
-               end
+               self.build_hash('get_messages', dyno, (c_time.to_f+s_time.to_f))
            end
 
            if l =~ /path=\/api\/users\/.*\/get_friends_progress (.*)/
 
-               if @final_hash['friends_progress']
-                   if @final_hash['friends_progress']['dyno']
-                       @final_hash['friends_progress']['dyno'].push(dyno)
-                       @final_hash['friends_progress']['response_time'].push(c_time.to_f + s_time.to_f)
-                   end
-               end
+               self.build_hash('friends_progress', dyno, (c_time.to_f+s_time.to_f))
            end
 
            if l =~ /path=\/api\/users\/.*\/get_friends_score (.*)/
 
-               if @final_hash['friends_score']
-                   if @final_hash['friends_score']['dyno']
-                       @final_hash['friends_score']['dyno'].push(dyno)
-                       @final_hash['friends_score']['response_time'].push(c_time.to_f + s_time.to_f)
-                   end
-               end
+               self.build_hash('friends_score', dyno, (c_time.to_f+s_time.to_f))
            end
 
            if l =~ /method=POST path=\/api\/users\/\d+ (.*)/
 
-               if @final_hash['post_users']
-                   if @final_hash['post_users']['dyno']
-                       @final_hash['post_users']['dyno'].push(dyno)
-                       @final_hash['post_users']['response_time'].push(c_time.to_f + s_time.to_f)
-                   end
-               end
+               self.build_hash('post_users', dyno, (c_time.to_f+s_time.to_f))
            end
 
            if l =~ /method=GET path=\/api\/users\/\d+ (.*)/
 
-               if @final_hash['get_users']
-                   if @final_hash['get_users']['dyno']
-                       @final_hash['get_users']['dyno'].push(dyno)
-                       @final_hash['get_users']['response_time'].push(c_time.to_f + s_time.to_f)
-                   end
-               end
+               self.build_hash('get_users', dyno, (c_time.to_f+s_time.to_f))
            end
        }
        
     end
 
-    def get_results
-        self.prepare_hash
-        @final_hash.each do |key, value|
+    def build_hash(root_hash, dyno, response_time)
+        '''
+        Push data into parent hash
+        '''
+        @final_hash[root_hash]['dyno'].push(dyno)
+        @final_hash[root_hash]['response_time'].push(response_time)
+    end
 
+    def get_results
+        '''
+        This method is responsible to print output at console
+        '''
+        self.prepare_hash
+        
+        @final_hash.each do |key, value|
+            path = value['path']
             total_hit = value['dyno'].length
             url_dyno = ''
             most_common_value(value['dyno']).each do |ke,va| url_dyno << "#{ke}," end
@@ -138,9 +127,9 @@ class LogfileParser
             url_mode = url_mode.chomp(',')
 
             if total_hit > 0
-                puts "'#{key}' url called #{total_hit} times, mean=#{url_mean}, median=#{url_median} and mode(s)=#{url_mode}. Most frequent dyno(s)='#{url_dyno}'"
+                puts "'#{path}' url called #{total_hit} times, mean=#{url_mean}, median=#{url_median} and mode(s)=#{url_mode}. Most frequent dyno(s)='#{url_dyno}'"
             else
-                puts "#{key}' did not call by users"
+                puts "'#{path}' did not call by users"
             end
 
         end
@@ -153,10 +142,4 @@ end
 
 parser = LogfileParser.new
 
-# parser.get_matched_lines {|l| puts l}
 parser.get_results
-
-
-# m_str = ''
-# mode([1,2,3,4,2,4]).each do |k,v| m_str << "#{k}," end
-# puts m_str.chomp(',')
